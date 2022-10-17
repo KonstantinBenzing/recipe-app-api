c6059ed9ea4f995b8456044105ceae5bc1933304
Costa Benzing <costa.benzing@student.odisee.be>
2022-10-17 14:08:37 +0200

    corrected calc.py comments

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-Calculator functiona
+Calculator functions
 """
 
 
 def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
 
 def subtract(x, y):
-    return x - y
+    """Subtract x from y and return result."""
+    return y - x
